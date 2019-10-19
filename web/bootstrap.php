f158cac3fe76d387f44958f8862815dbdde8572b
TiguerchaD <tiguercha.pro@gmail.com>
2019-10-19 16:10:24 +0200

    ...

--- web/bootstrap.php
+++ web/bootstrap.php
@@ -3,8 +3,12 @@
 
 use GuzzleHttp\Psr7\ServerRequest;
 
+const DEFAULT_APP = 'Frontend';
 
-require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require '../vendor/autoload.php';
+
+//require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
 //require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'OpenFram' . DIRECTORY_SEPARATOR . 'functions.php';
 
 
@@ -12,7 +16,6 @@ require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_
 $get = ServerRequest::fromGlobals()->getQueryParams()['app'] ?? false;
 
 
-const DEFAULT_APP = 'Frontend';
 
 if(!$get || !file_exists(__DIR__.'/../src/App/'.$get)) {
     $get['app'] = DEFAULT_APP;
