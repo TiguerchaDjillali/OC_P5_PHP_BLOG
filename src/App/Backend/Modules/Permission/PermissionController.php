356ff7c17af5c144652fcd23f70d35948029b662
TiguerchaD <tiguercha.pro@gmail.com>
2019-10-20 14:00:52 +0200

    ...

--- src/App/Backend/Modules/Permission/PermissionController.php
+++ src/App/Backend/Modules/Permission/PermissionController.php
@@ -2,14 +2,15 @@
 
 namespace App\Backend\Modules\Permission;
 
-
 use GuzzleHttp\Psr7\Request;
 use OpenFram\BackController;
 
 class PermissionController extends BackController
 {
 
-
+    /**
+     * @param Request $request
+     */
     public function executeIndex(Request $request)
     {
         $this->page->addVar('title', 'Permissions');
